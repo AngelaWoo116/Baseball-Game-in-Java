6ca6ab1e3c179443df38a903936c730f2661fdcc
AngelaWoo116 <39450039+AngelaWoo116@users.noreply.github.com>
2018-09-07 16:29:38 -0400

    Add files via upload

---
+++ BaseBallMain.java
@@ -0,0 +1,43 @@
+package com.hk.main;
+
+import com.hk.Base;
+import com.hk.BaseBall_Game;
+import com.hk.Hitter;
+import com.hk.Pitcher;
+import com.hk.Umpire;
+
+public class BaseBallMain {
+
+	public static void main(String[] args) {
+		
+		BaseBall_Game game = new BaseBall_Game();
+		game.newGame();
+		
+		
+//		Pitcher p = new Pitcher();
+//		p.make();
+//		int[] gong =  p.getGong();
+//		p.print();
+//		String gongStr =  p.toString();
+//		System.out.println(gongStr);
+		
+//		Hitter hit = new Hitter();
+//		hit.make();
+//		hit.print();
+		
+//		Base p = new Hitter();
+//		p.make();
+//		p.print();
+//		
+//		Umpire u = new Umpire();
+//		u.sethBall(new int[]{1,2,3});
+//		u.setpBall(new int[]{3,2,1});
+//		System.out.println(u.strike());
+//		System.out.println(u.ball());
+		
+
+		
+		
+	}
+
+}
